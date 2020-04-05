848f07eac9b39b387f849c6e3d06ac6ce3244f1e
githubyosshi <kanrisya.shisutemu@gmail.com>
2020-04-05 15:21:34 +0900

    MainActivity class Function description 2 commit

--- app/src/main/java/com/brown_chicken/diary01/MainActivity.java
+++ app/src/main/java/com/brown_chicken/diary01/MainActivity.java
@@ -1,13 +1,19 @@
 package com.brown_chicken.diary01;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static int REQUEST_CODE = 0;    //リクエストコード
@@ -29,8 +35,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lastMonthButton.setOnClickListener(this);
 
         //「来月」ボタンのオブジェクトを取得とイベントリスナ登録
-        Button nextMontButton = (Button)findViewById(R.id.nextMonthButton);
-        nextMontButton.setOnClickListener(this);
+        Button nextMonthButton = (Button)findViewById(R.id.nextMonthButton);
+        nextMonthButton.setOnClickListener(this);
 
         //MyCalendarクラスのオブジェクトを取得
         myCalendar = MyCalendar.getInstance();
@@ -52,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
 
         switch (id) {
-            case R.id.lastMonthButton: //「前月」ボタンを押した場合
+            case R.id.lastMonthButton : //「前月」ボタンを押した場合
                 //月の情報を−１する
                 myCalendar.setLastMonth();
 
@@ -60,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 displayAll();
 
                 break;
-            case R.id.nextMonthButton: //「来月」ボタンを押した場合
+            case R.id.nextMonthButton : //「来月」ボタンを押した場合
                 //月の情報を＋１する
                 myCalendar.setNextMonth();
 
@@ -68,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 displayAll();
 
                 break;
-            default: //「日」ボタンを押した場合
+            default : //「日」ボタンを押した場合
                 //day.xmlのタグ情報を取得
                 String code = (String) view.getTag();
 
@@ -86,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void omActivityResult(int requestCode, int resultCode, Intent intent) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
         //インテントに設定した情報を取得
@@ -106,5 +112,128 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             t.show();
         }
     }
-    
+
+    private void displayAll() {
+        //GridLayout内に設定している全てのビューを削除
+        gridLayout.removeAllViews();
+
+        //年月を「○○○○年□□月」の書式で取得
+        String ymData = myCalendar.getFormat(1);
+
+        //年月表示用のTextViewを取得
+        TextView ymText = (TextView)findViewById(R.id.ymText);
+
+        //年月表示用のTextViewに年月を表示
+        ymText.setText(ymData);
+
+        //displayWeeksメソッドを呼び出す
+        displayWeeks();
+
+        //displayDaysメソッドを呼び出す
+        displayDays();
+    }
+
+    private void displayWeeks() {
+        String[] weeks = {"日", "月", "火", "水", "木", "金", "土"};
+
+        for (String week : weeks) {
+            //GridLayoutに設定する曜日情報表示用のビュー（week.xml）を取得
+            View child = getLayoutInflater().inflate(R.layout.week, null);
+
+            //week.xmlに設定した曜日表示用のTextViewを取得
+            TextView text = (TextView)child.findViewById(R.id.weekText);
+
+            //曜日表示用のTextViewに配列weeksの文字を設定
+            text.setText(week);
+
+            //GridLayout.LayoutParamsのオブジェクトを生成
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+
+            //GridLayout.LayoutParamsオブジェクトの幅をコンテナのサイズに合わせる
+            params.setGravity(Gravity.FILL_HORIZONTAL);
+
+            //GridLayoutにビュー（weeks.xml）を追加
+            gridLayout.addView(child);
+        }
+    }
+
+    private void displayDays() {
+        //getCodeListメソッドを呼び出しコードリストを取得
+        ArrayList<String> codeList = getCodeList();
+
+        //設定された月の最大日数を取得
+        int max = myCalendar.getMax();
+
+        //設定された曜日の情報を取得
+        int week = myCalendar.getWeek();
+
+        for (int i = 1; i <= max; i++) {    //１〜月の最大日数まで繰り返す
+            //GridLayoutに設定する日および状態表示用のビュー（day.xml）を取得
+            View child = getLayoutInflater().inflate(R.layout.day, null);
+
+            //day.xmlに設定した日表示用のTextViewを取得
+            TextView dayText = (TextView) child.findViewById(R.id.dayText);
+
+            //日表示用のTextViewに日情報を設定
+            dayText.setText(Integer.toString(i));
+
+            //日情報を元に年月日情報を取得
+            String ymdData = myCalendar.getCode(i);
+
+            //day.xmlに設定した状態表示用のTextViewを取得
+            TextView dispText = (TextView) child.findViewById(R.id.dispText);
+
+            //コードリストに一致する年月日情報が存在すれば
+            //状態表示用のTextViewに文字列「＊日記あり」を表示
+            dispText.setText(codeList.contains(ymdData) ? "＊日記あり" : "");
+
+            //day.xmlに年月日情報をタグとして設定
+            child.setTag(ymdData);
+
+            //day.xmlを押した時のイベントリスナ登録
+            child.setOnClickListener(this);
+
+            //GridLayout.LayoutParamsのオブジェクトを生成
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+
+            if (i == 1) {
+                //「１」を表示する開始位置を設定
+                params.rowSpec = GridLayout.spec(1);    //「１」行目
+                params.columnSpec = GridLayout.spec(week - 1);  //「week - 1」列目
+            }
+
+            //GridLayout.LayoutParamsオブジェクトの幅をコンテナのサイズに合わせる
+            params.setGravity(Gravity.FILL_HORIZONTAL);
+
+            //GridLayoutにビュー（days.xmlとパラメータ）を追加
+            gridLayout.addView(child, params);
+        }
+    }
+
+    private ArrayList<String> getCodeList() {
+        //年月日情報のコードの文字列を格納用ArrayList
+        ArrayList<String> codeList = new ArrayList<String>();
+
+        //年月を「○○○○□□」の書式で取得
+        String ymData = myCalendar.getFormat(2);
+
+        //SQLiteDatabaseクラスを取得（読込み用）
+        SQLiteDatabase database = helper.getReadableDatabase();
+
+        //Diariesテーブルからデータを取得する列を設定
+        String[] columns = {"CODE"};
+
+        //年月部分が一致するレコードを取得するように条件設定
+        String selection = "code LIKE '" + ymData + "%'";
+
+        //Diariesテーブルから条件に一致したcode情報を取得
+        Cursor cursor = database.query("Diaries", columns, selection, null, null, null, null);
+
+        //取得したcodeをArrayListに順次格納
+        while (cursor.moveToNext()) {
+            codeList.add(cursor.getString(0));
+        }
+        //コードリストを返す
+        return codeList;
+    }
 }
